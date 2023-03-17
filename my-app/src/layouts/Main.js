0d70b637237b133345cfc03da191909185c74f31
Mary <mary.pommer@tptlive.ee>
2023-03-17 16:20:58 +0200

    add about container

--- my-app/src/layouts/Main.js
+++ my-app/src/layouts/Main.js
@@ -39,7 +39,7 @@ const Main = () => {
                         />
                     </Col>
                     <Col lg={5}>
-                        <div>
+                        <div className="AboutContainer">
                             <h2>About our furniture</h2>
                             <p>Our multifunctional collection blends design and function to suit your individual taste. Make each room unique, or pick a cohesive theme that best express your interests and what inspires you. Find the furniture pieces you need, from traditional to contemporary styles or anything in between. Product specialists are available to help you create your dream space.</p>
                         </div>
@@ -62,7 +62,7 @@ const Main = () => {
                     src={LeftPic}
                     alt="Third slide"
                 />
-                <div>
+                <div className="AboutContainer">
                     <h2>About our furniture</h2>
                     <p>Our multifunctional collection blends design and function to suit your individual taste. Make each room unique, or pick a cohesive theme that best express your interests and what inspires you. Find the furniture pieces you need, from traditional to contemporary styles or anything in between. Product specialists are available to help you create your dream space.</p>
                 </div>
