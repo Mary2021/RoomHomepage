512582d62045526503d00ce9bb5e6790215013b0
Mary <mary.pommer@tptlive.ee>
2023-03-21 15:38:52 +0200

    add hidden mobile menu

--- src/layouts/NavBar.js
+++ src/layouts/NavBar.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react'
+import Container from 'react-bootstrap/Container'
 import logo from './../images/logo.svg'
 import hb from './../images/icon-hamburger.svg'
+import x from './../images/icon-close.svg'
 
 const NavBar = () => {
     let [open, setOpen] = useState(false)
@@ -27,23 +29,34 @@ const NavBar = () => {
     document.className = open ? 'visible' : 'hidden';
 
     if (window.innerWidth >= 376) {
-    return (
-        <header className='Menu'>
-            <img src={logo} alt='logo'className='Logo' />
-            <a className='MenuLink'>Home</a>
-            <a className='MenuLink'>Shop</a>
-            <a className='MenuLink'>About</a>
-            <a className='MenuLink'>Contact</a>
-        </header>
-    )
+        return (
+            <header className='Menu'>
+                <img src={logo} alt='logo'className='Logo' />
+                <a className='MenuLink'>Home</a>
+                <a className='MenuLink'>Shop</a>
+                <a className='MenuLink'>About</a>
+                <a className='MenuLink'>Contact</a>
+            </header>
+        )
     } else {
+        console.log(open)
+        console.log(document.className)
         return(
-        <header className='Menu'>
-            <img  src={hb} className="MenuIcon" alt="MenuIcon" onClick={() => setOpen(!open)}/>
-            <img src={logo} alt='logo'className='Logo' />
-            {/* <div className={document.className}>
-            </div> */}
-        </header>
+            <header className='Menu'>
+                <img src={hb} className="MenuIcon" alt="MenuIcon" onClick={() => setOpen(!open)}/>
+                <img src={logo} alt='logo' className='Logo' />
+                <div className={document.className}>
+                    <Container>
+                        <img src={x} className="CloseIcon" alt="CloseIcon" onClick={() => setOpen(!open)}/>
+                        <div className='LinkContainer'>
+                            <a className='MenuLink'>Home</a>
+                            <a className='MenuLink'>Shop</a>
+                            <a className='MenuLink'>About</a>
+                            <a className='MenuLink'>Contact</a>
+                        </div>
+                    </Container>
+                </div>
+            </header>
         )
     }
 }
